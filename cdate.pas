154f028bff3e876a38b0e377490271096def8bc9
Aleksei Faians <af.git@bsrealm.net>
2024-01-02 16:12:59 +0800

    Misc

--- cdate.pas
+++ cdate.pas
@@ -1,16 +1,12 @@
 {$I+}
 Uses
+  tMisc,
   DOS;
 
 Var
-  F                             : Text;
-  Year, Month, Day, DayOfWeek   : Word;
-  tS, S                         : String [16];
-
-Const
-  Months : Array [1..12] Of String [3] =
-           ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-            'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec');
+  F           : Text;
+  DayOfWeek   : Word;
+  DT          : DateTime;
 
 Begin
   If ParamCount = 0 Then
@@ -18,13 +14,12 @@ Begin
     WriteLn ('Usage: ' + ParamStr (0) + '[[drive:\]path\to\]cdate.inc');
     Halt (1);
   End;
-  GetDate (Year, Month, Day, DayOfWeek);
-  Str (Day, tS);
-  S := '''' + tS + '-' + Months [Month] + '-';
-  Str (Year, tS);
-  S := S + tS + '''';
+
+  With DT Do
+    GetDate (Year, Month, Day, DayOfWeek);
+
   Assign (F, ParamStr (1));
   ReWrite (F);
-  WriteLn (F, S);
+  WriteLn (F, FormattedDate (DT, '''DD-NNN-YYYY'''));
   Close (F);
 End.
