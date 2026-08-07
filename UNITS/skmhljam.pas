shadowlmd/tornado-bbs

commit a59bb4027d48f79c7dd1f3d092f5d2e40e7cc38a

Replace literal ^A character with #1.

UNITS/skmhljam.pas
@@ -581,11 +581,11 @@ function TJamMessageBase.OpenMessage: Boolean;
   while GetSubField(Count, SubFieldType, S) do
    begin
     case SubFieldType of
-     jamMSGID: PutString('MSGID: ' + S);
-     jamREPLY: PutString('REPLY: ' + S);
-     jamPID: PutString('PID: ' + S);
-     jamUnknown: PutString('' + S);
-     jamFLAGS: PutString('FLAGS: ' + S);
+     jamMSGID: PutString(#1'MSGID: ' + S);
+     jamREPLY: PutString(#1'REPLY: ' + S);
+     jamPID: PutString(#1'PID: ' + S);
+     jamUnknown: PutString(#1 + S);
+     jamFLAGS: PutString(#1'FLAGS: ' + S);
     end;
     Inc(Count);
    end;
@@ -596,7 +596,7 @@ function TJamMessageBase.OpenMessage: Boolean;
   while GetSubField(Count, SubFieldType, S) do
    begin
     if SubFieldType = jamVia then
-     PutString('Via ' + S);
+     PutString(#1'Via ' + S);
 
     Inc(Count);
    end;
@@ -614,7 +614,7 @@ function TJamMessageBase.OpenMessage: Boolean;
   while GetSubField(Count, SubFieldType, S) do
    begin
     if SubFieldType = jamPATH then
-     PutString('PATH: ' + S);
+     PutString(#1'PATH: ' + S);
 
     Inc(Count);
    end;
@@ -713,11 +713,11 @@ function TJamMessageBase.OpenMessageHeader: Boolean;
   while GetSubField(Count, SubFieldType, S) do
    begin
     case SubFieldType of
-     jamMSGID: PutString('MSGID: ' + S);
-     jamREPLY: PutString('REPLY: ' + S);
-     jamPID: PutString('PID: ' + S);
-     jamUnknown: PutString('' + S);
-     jamFLAGS: PutString('FLAGS: ' + S);
+     jamMSGID: PutString(#1'MSGID: ' + S);
+     jamREPLY: PutString(#1'REPLY: ' + S);
+     jamPID: PutString(#1'PID: ' + S);
+     jamUnknown: PutString(#1 + S);
+     jamFLAGS: PutString(#1'FLAGS: ' + S);
     end;
 
     Inc(Count);
@@ -895,27 +895,27 @@ function TJamMessageBase.WriteMessage: Boolean;
     LineLength:=LenASCIIZ(Line) and $FF;
 
     Move(Line^, S[1], LineLength);
-    if Copy(S, 1, 8) = 'MSGID: ' then
+    if Copy(S, 1, 8) = #1'MSGID: ' then
      begin
       AddSubField(jamMSGID, Copy(S, 9, 255));
 
       JamMessageHeader.JamHeader.MSGIDCrc:=StrCrc32(Copy(S, 9, 255));
      end else
-    if Copy(S, 1, 8) = 'REPLY: ' then
+    if Copy(S, 1, 8) = #1'REPLY: ' then
      begin
       AddSubField(jamREPLY, Copy(S, 9, 255));
 
       JamMessageHeader.JamHeader.REPLYCrc:=StrCrc32(Copy(S, 9, 255));
      end else
-    if Copy(S, 1, 6) = 'PID: ' then AddSubField(jamPID, Copy(S, 7, 255)) else
-    if Copy(S, 1, 8) = 'FLAGS: ' then AddSubField(jamFLAGS, Copy(S, 9, 255)) else
-    if Copy(S, 1, 5) = 'Via ' then AddSubField(jamVia, Copy(S, 6, 255)) else
+    if Copy(S, 1, 6) = #1'PID: ' then AddSubField(jamPID, Copy(S, 7, 255)) else
+    if Copy(S, 1, 8) = #1'FLAGS: ' then AddSubField(jamFLAGS, Copy(S, 9, 255)) else
+    if Copy(S, 1, 5) = #1'Via ' then AddSubField(jamVia, Copy(S, 6, 255)) else
     if Copy(S, 1, 9) = 'SEEN-BY: ' then AddSubField(jamSEENBY, Copy(S, 10, 255)) else
-    if Copy(S, 1, 7) = 'PATH: ' then AddSubField(jamPATH, Copy(S, 8, 255)) else
-    if Copy(S, 1, 5) = 'INTL' then else
-    if Copy(S, 1, 5) = 'FMPT' then else
-    if Copy(S, 1, 5) = 'TOPT' then else
-    if Copy(S, 1, 1) = '' then AddSubField(jamUnknown, Copy(S, 2, 255)) else
+    if Copy(S, 1, 7) = #1'PATH: ' then AddSubField(jamPATH, Copy(S, 8, 255)) else
+    if Copy(S, 1, 5) = #1'INTL' then else
+    if Copy(S, 1, 5) = #1'FMPT' then else
+    if Copy(S, 1, 5) = #1'TOPT' then else
+    if S[1] = #1 then AddSubField(jamUnknown, Copy(S, 2, 255)) else
      begin
       Buffer^.Write(Line^, LenASCIIZ(Line));
 

UNITS/skmhl.pas
@@ -555,21 +555,21 @@ procedure TMessageBase.SetFromAddress(var Address: TAddress; const FreshMSGID: B
   ToAddress: TAddress;
   S: String;
  begin
-  if (not FreshMSGID) and GetKludge('MSGID:', S) then
-   SetKludge('MSGID:', 'MSGID: ' + AddressToStrEx(Address) + ' ' + ExtractWord(3, S, [' ']))
+  if (not FreshMSGID) and GetKludge(#1'MSGID:', S) then
+   SetKludge(#1'MSGID:', #1'MSGID: ' + AddressToStrEx(Address) + ' ' + ExtractWord(3, S, [' ']))
   else
-   SetKludge('MSGID:', 'MSGID: ' + AddressToStrEx(Address) + ' ' + GenerateMSGID);
+   SetKludge(#1'MSGID:', #1'MSGID: ' + AddressToStrEx(Address) + ' ' + GenerateMSGID);
 
   if GetFlag(afNetmail) then
    begin
     GetToAddress(ToAddress);
 
-    SetKludge('INTL', 'INTL ' + AddressToStrPointless(ToAddress) + ' ' + AddressToStrPointless(Address));
+    SetKludge(#1'INTL', #1'INTL ' + AddressToStrPointless(ToAddress) + ' ' + AddressToStrPointless(Address));
 
     if Address.Point = 0 then
-     DeleteKludge('FMPT')
+     DeleteKludge(#1'FMPT')
     else
-     SetKludge('FMPT', 'FMPT ' + LongToStr(Address.Point));
+     SetKludge(#1'FMPT', #1'FMPT ' + LongToStr(Address.Point));
    end;
  end;
 
@@ -581,12 +581,12 @@ procedure TMessageBase.SetToAddress(var Address: TAddress);
    begin
     GetFromAddress(FromAddress);
 
-    SetKludge('INTL', 'INTL ' + AddressToStrPointless(Address) + ' ' + AddressToStrPointless(FromAddress));
+    SetKludge(#1'INTL', #1'INTL ' + AddressToStrPointless(Address) + ' ' + AddressToStrPointless(FromAddress));
 
     if Address.Point = 0 then
-     DeleteKludge('TOPT')
+     DeleteKludge(#1'TOPT')
     else
-     SetKludge('TOPT', 'TOPT ' + LongToStr(Address.Point));
+     SetKludge(#1'TOPT', #1'TOPT ' + LongToStr(Address.Point));
    end;
  end;
 
@@ -981,8 +981,8 @@ procedure TMessageBase.CheckFromAddress(const S: String; var Address: TAddress);
  var
   A: TAddress;
  begin
-  if Copy(S, 1, 7) = 'MSGID:' then StrToAddress(ExtractWord(2, S, [' ']), Address);
-  if Copy(S, 1, 5) = 'INTL'   then
+  if Copy(S, 1, 7) = #1'MSGID:' then StrToAddress(ExtractWord(2, S, [' ']), Address);
+  if Copy(S, 1, 5) = #1'INTL'   then
    begin
     A.Point:=Address.Point;
 
@@ -990,14 +990,14 @@ procedure TMessageBase.CheckFromAddress(const S: String; var Address: TAddress);
 
     Address.Point:=A.Point;
    end else
-  if Copy(S, 1, 5) = 'FMPT'   then StrToInteger(ExtractWord(2, S, [' ']), Address.Point);
+  if Copy(S, 1, 5) = #1'FMPT'   then StrToInteger(ExtractWord(2, S, [' ']), Address.Point);
  end;
 
 procedure TMessageBase.CheckToAddress(const S: String; var Address: TAddress);
  var
   A: TAddress;
  begin
-  if Copy(S, 1, 5) = 'INTL' then
+  if Copy(S, 1, 5) = #1'INTL' then
    begin
     A.Point:=Address.Point;
 
@@ -1005,7 +1005,7 @@ procedure TMessageBase.CheckToAddress(const S: String; var Address: TAddress);
 
     Address.Point:=A.Point;
    end else
-  if Copy(S, 1, 5) = 'TOPT' then StrToInteger(ExtractWord(2, S, [' ']), Address.Point);
+  if Copy(S, 1, 5) = #1'TOPT' then StrToInteger(ExtractWord(2, S, [' ']), Address.Point);
  end;
 
 function TMessageBase.CreateNewMessage: Boolean;

commit 9ea1352513ce0af25aa3b71eff55b79f22641fd0

Fix bug introduced in a59bb4027d48f79c7dd1f3d092f5d2e40e7cc38a - accesing String by index returns a value even if String length is zero, this caused empty jamUnknown kludges appearing instead of newlines after kludge lines

UNITS/skmhljam.pas
@@ -918,7 +918,7 @@ function TJamMessageBase.WriteMessage: Boolean;
     if Copy(S, 1, 5) = #1'INTL' then else
     if Copy(S, 1, 5) = #1'FMPT' then else
     if Copy(S, 1, 5) = #1'TOPT' then else
-    if S[1] = #1 then AddSubField(jamUnknown, Copy(S, 2, 255)) else
+    if Copy(S, 1, 1) = #1 then AddSubField(jamUnknown, Copy(S, 2, 255)) else
      begin
       Buffer^.Write(Line^, LenASCIIZ(Line));
 